288f76e0d5046bb7e9cc2708f526f5d5eed32278
Brad Garropy <bradgarropy@gmail.com>
2015-05-18 23:10:44 -0500

    #6 Retrieve weight and body fat measurements
    Used the MyFitnessPal library to retrieve the weight and body fat
    measurement dictionaries.

--- macrolyzer.py
+++ macrolyzer.py
@@ -4,3 +4,20 @@ macrolyzer.py
 
 Module that tracks weight trends over time.
 """
+
+
+import day
+import datetime
+import myfitnesspal
+
+# create MyFitnessPal client
+client = myfitnesspal.Client('username','password')
+
+# establish a 5 week date range
+today = datetime.date.today()
+past = today - datetime.timedelta(weeks=5)
+
+# retrieve weight and body fat measurements
+weight = client.get_measurements('Weight', today, past)
+body_fat = client.get_measurements('Body Fat', today, past)
+
