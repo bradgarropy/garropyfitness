01a7b45ad9f05d2723f873949ddb52f666c273b1
Brad Garropy <bradgarropy@gmail.com>
2015-05-19 00:18:40 -0500

    #9 Added a main method
    Placed all of the macrolyzer code under the main method and added the
    obligatory if statement at the bottom of the file. Still need to add
    support for username and password arguments.

--- macrolyzer.py
+++ macrolyzer.py
@@ -10,33 +10,39 @@ import day
 import datetime
 import myfitnesspal
 
-# create MyFitnessPal client
-client = myfitnesspal.Client('username','password')
 
-# establish a 5 week date range
-today = datetime.date.today()
-past = today - datetime.timedelta(weeks=5)
+def main():
+    """Retrieves and analyzes weight and body fat from MyFitnessPal."""
 
-# retrieve weight and body fat measurements
-weight = client.get_measurements('Weight', today, past)
-body_fat = client.get_measurements('Body Fat', today, past)
+    # create MyFitnessPal client
+    client = myfitnesspal.Client('username', 'password')
 
-# calculate the date range
-date_range = (today - past).days
+    # establish a 5 week date range
+    today = datetime.date.today()
+    past = today - datetime.timedelta(weeks=5)
 
-days = []
+    # retrieve weight and body fat measurements
+    weight = client.get_measurements('Weight', today, past)
+    body_fat = client.get_measurements('Body Fat', today, past)
 
-# loop over each day in the range
-for delta in range(0, date_range + 1):
+    # calculate the date range
+    date_range = (today - past).days
 
-    # retrieve the values for the current day object
-    current_date = today - datetime.timedelta(days=delta)
-    current_weight = weight.get(current_date, None)
-    current_body_fat = body_fat.get(current_date, None)
+    days = []
 
-    # create the day object
-    current_day = day.Day(current_date, current_weight, current_body_fat)
+    # loop over each day in the range
+    for delta in range(0, date_range + 1):
 
-    # add the current day to the list of days
-    days.append(current_day)
+        # retrieve the values for the current day object
+        current_date = today - datetime.timedelta(days=delta)
+        current_weight = weight.get(current_date, None)
+        current_body_fat = body_fat.get(current_date, None)
 
+        # create the day object
+        current_day = day.Day(current_date, current_weight, current_body_fat)
+
+        # add the current day to the list of days
+        days.append(current_day)
+
+if __name__ == "__main__":
+    main()
