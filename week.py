5fba313b1ba94ff654cbb6498527878b9ca95fc7
Brad Garropy <bradgarropy@gmail.com>
2015-05-19 22:07:32 -0500

    #5 Improve week class
    Added variables for weight and body fat change. Weight and body fat
    averages are now calculated when a week object is instantiated.

--- week.py
+++ week.py
@@ -12,3 +12,17 @@ class Week(object):
         self.days = days
         self.weight_avg = 0
         self.body_fat_avg = 0
+        self.weight_delta = 0
+        self.body_fat_delta = 0
+
+        # calculate weight average
+        for day in days:
+            self.weight_avg += day.weight
+
+        self.weight_avg = self.weight_avg/len(days)
+
+        # calculate body fat average
+        for day in days:
+            self.body_fat_avg += day.body_fat
+
+        self.body_fat_avg = self.body_fat_avg/len(days)
