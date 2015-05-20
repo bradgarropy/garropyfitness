43a9fb358fa4a1221a65a1f490062bfd0a56def5
Brad Garropy <bradgarropy@gmail.com>
2015-05-20 17:45:07 -0500

    #13 Added support for lean body mass and fat mass
    Modified the day and the week object to include lean body mass and fat
    mass. Averages are calculated for both.

--- day.py
+++ day.py
@@ -12,3 +12,13 @@ class Day(object):
         self.date = date
         self.weight = weight
         self.body_fat = body_fat
+        self.fat_mass = 0
+        self.lean_body_mass = 0
+
+        # calculate fat mass
+        self.fat_mass = self.weight * (self.body_fat / 100)
+        self.fat_mass = round(self.fat_mass, 1)
+
+        # calculate lean body mass
+        self.lean_body_mass = self.weight - self.fat_mass
+        self.lean_body_mass = round(self.lean_body_mass, 1)
--- week.py
+++ week.py
@@ -10,19 +10,43 @@ class Week(object):
 
     def __init__(self, days):
         self.days = days
+
+        # averages
         self.weight_avg = 0
         self.body_fat_avg = 0
+        self.fat_mass_avg = 0
+        self.lean_body_mass_avg = 0
+
+        # deltas
         self.weight_delta = 0
         self.body_fat_delta = 0
+        self.fat_mass_delta = 0
+        self.lean_body_mass_delta = 0
 
         # calculate weight average
         for day in days:
             self.weight_avg += day.weight
 
         self.weight_avg = self.weight_avg/len(days)
+        self.weight_avg = round(self.weight_avg, 1)
 
         # calculate body fat average
         for day in days:
             self.body_fat_avg += day.body_fat
 
         self.body_fat_avg = self.body_fat_avg/len(days)
+        self.body_fat_avg = round(self.body_fat_avg, 1)
+
+        # calculate fat mass average
+        for day in days:
+            self.fat_mass_avg += day.fat_mass
+
+        self.fat_mass_avg = self.fat_mass_avg/len(days)
+        self.fat_mass_avg = round(self.fat_mass_avg, 1)
+
+        # calculate lean body mass average
+        for day in days:
+            self.lean_body_mass_avg += day.lean_body_mass
+
+        self.lean_body_mass_avg = self.lean_body_mass_avg/len(days)
+        self.lean_body_mass_avg = round(self.lean_body_mass_avg, 1)
