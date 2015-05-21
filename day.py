f2322d869967b083771e5c9c5c9698f5a1a0e9aa
Brad Garropy <bradgarropy@gmail.com>
2015-05-20 22:53:47 -0500

    #15 Skip calculations when values are empty
    Initialize fat mass and lean body mass to None. Only calculate these
    values if a weight and body fat are recorded for that day.

--- day.py
+++ day.py
@@ -12,13 +12,15 @@ class Day(object):
         self.date = date
         self.weight = weight
         self.body_fat = body_fat
-        self.fat_mass = 0
-        self.lean_body_mass = 0
+        self.fat_mass = None
+        self.lean_body_mass = None
 
-        # calculate fat mass
-        self.fat_mass = self.weight * (self.body_fat / 100)
-        self.fat_mass = round(self.fat_mass, 1)
+        # calulate only if weight and body fat have values
+        if weight and body_fat:
+            # calculate fat mass
+            self.fat_mass = self.weight * (self.body_fat / 100)
+            self.fat_mass = round(self.fat_mass, 1)
 
-        # calculate lean body mass
-        self.lean_body_mass = self.weight - self.fat_mass
-        self.lean_body_mass = round(self.lean_body_mass, 1)
+            # calculate lean body mass
+            self.lean_body_mass = self.weight - self.fat_mass
+            self.lean_body_mass = round(self.lean_body_mass, 1)
