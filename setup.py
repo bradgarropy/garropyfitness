bb1a91968bb00787edac0a318718c73fa25800b5
Brad Garropy <bradgarropy@gmail.com>
2016-04-18 23:31:01 -0500

    Packaged macrolyzer
    Added a setup script for the macrolyzer package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='macrolyzer',
+      version='0.1.0',
+      description='Macronutrient and weight trend analyzer',
+      url='https://github.com/bradgarropy/macrolyzer',
+      author='Brad Garropy',
+      author_email='bradgarropy@gmail.com',
+      license='MIT',
+      packages=['macrolyzer'],
+      install_requires=['myfitnesspal'],
+      zip_safe=False)
